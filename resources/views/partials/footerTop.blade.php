fc5ceab0075c765e1fda4698d84ea17b09e16574
CrisL1990 <lucchetta.cris@gmail.com>
2022-03-29 15:04:38 +0200

    Inserito link al css

--- resources/views/partials/footerTop.blade.php
+++ resources/views/partials/footerTop.blade.php
@@ -4,23 +4,23 @@
         <section id="footer-top">
 
             <div class="box">
-                <a href="#"><img id="visa" src="{{asset('img/buy-dc-power-visa.svg')}}" alt="DC logo footer"></a>
-                <span>DC POWER VISA</span>
+                <a href="#"><img id="visa" src="{{asset('img/buy-comics-digital-comics.png')}}" alt="DC logo footer"></a>
+                <span>DIGITAL COMICS</span>
             </div>
 
             <div class="box">
-                <a href="#"><img id="visa" src="{{asset('img/buy-dc-power-visa.svg')}}" alt="DC logo footer"></a>
-                <span>DC POWER VISA</span>
+                <a href="#"><img id="visa" src="{{asset('img/buy-comics-merchandise.png')}}" alt="DC logo footer"></a>
+                <span>DC MERCHANDISE</span>
             </div>
 
             <div class="box">
-                <a href="#"><img id="visa" src="{{asset('img/buy-dc-power-visa.svg')}}" alt="DC logo footer"></a>
-                <span>DC POWER VISA</span>
+                <a href="#"><img id="visa" src="{{asset('img/buy-comics-subscriptions.png')}}" alt="DC logo footer"></a>
+                <span>SUBSCRIPTION</span>
             </div>
 
             <div class="box">
-                <a href="#"><img id="visa" src="{{asset('img/buy-dc-power-visa.svg')}}" alt="DC logo footer"></a>
-                <span>DC POWER VISA</span>
+                <a href="#"><img id="visa" src="{{asset('img/buy-comics-shop-locator.png')}}" alt="DC logo footer"></a>
+                <span>COMIC SHOP LOCATOR</span>
             </div>
 
             <div class="box">
